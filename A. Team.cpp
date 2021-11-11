2b9d5b62459ae66140ff3602d55bf631887350e2
Ahmed Zaher <ahmedzaher5788@gmail.com>
2021-11-11 18:22:07 +0200

    Create A. Team.cpp

--- A. Team.cpp
+++ A. Team.cpp
@@ -7,14 +7,17 @@ int main()
     int n,counter=0,total=0;
     bool k;
     cin>>n;
-    for(int i=1;i<=n;i++;){
+    for(int i=1;i<=n;i++){
         for(int j=1;j<=3;j++){
             cin>>k;
-            if(k)
-                counter++;
+            if(k){
+               counter++;
+            }
+
         }
         if(counter>=2)
             total++;
+        counter=0;
     }
     cout<<total;
   return 0;
