d6fb85bd5bd1b8e8d1d651c15cb74612a9c958ad
Ahmed Zaher <ahmedzaher5788@gmail.com>
2021-11-18 18:56:06 +0200

    V2
    This is more optimized version than last one

--- A. Magnets.cpp
+++ A. Magnets.cpp
@@ -6,29 +6,15 @@ using namespace std;
 #include<string>
 int main()
 {
-    // 10--> - +  = {1} in arr
-    // 01--> +-  =  {0} in arr
-    int n,j,counter=0;
+    int n,t,j,counter=1;
     cin>>n;
-    int arr[n];
-    for(int i=0;i<n;i++){
+    cin>>t;
+    for(int i=1;i<n;i++){
         //cout<<"test"<<endl;
         cin>>j;
-        if(j==10)
-            arr[i]=1;
-        else
-            arr[i]=0;
+        if(t!=j)counter++;
+        t=j
     }
-    //we stared with second magnet comparing it with first magnet, that's why i=1
-    for(int i=1;i<n;i++){
-        if(arr[i]!=arr[i-1])
-            counter++;
-    }
-    cout<<counter+1;
-    /*
-    because if input is 6 10 10 10 01 10 10
-    array is: 1 1 1 0 1 1 --> arr[3]!=arr[2] & arr[4]!=arr[3]
-    so condition below is true twice, but we have 3 groups of magnets
-    */
+    cout<<counter;
     return 0;
 }
