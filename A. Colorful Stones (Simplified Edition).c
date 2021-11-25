fa9df513b1ffac70dbd9d3da3866b310084ba727
Ahmed Zaher <ahmedzaher5788@gmail.com>
2021-11-25 16:51:19 +0200

    Create A. Colorful Stones (Simplified Edition).c

---
+++ A. Colorful Stones (Simplified Edition).c
@@ -0,0 +1,38 @@
+#include <algorithm>
+#include <cassert>
+#include <cstdio>
+#include <cstdlib>
+#include <cctype>
+#include <cmath>
+#include <stack>
+#include <queue>
+#include <map>
+#include <set>
+#include <functional>
+#include <numeric>
+#include <iostream>
+#include <sstream>
+#include <string>
+#include <utility>
+#include <vector>
+#include <unistd.h>
+#include <cstring>
+using namespace std;
+int main()
+
+{
+    string s,t;
+    int i=0,j=0,curr=1;
+    cin>>s>>t;
+    while(i<s.size()&&j<t.size()){
+        if(s[i]==t[j]){
+            curr++;
+            i++;
+            j=j+1;
+        }
+        else
+            j++; // THIS MUSSSSSSSSSSST BE INSIDE ELSE STATMESNT
+        }
+    cout<<curr;
+	return 0;
+}
