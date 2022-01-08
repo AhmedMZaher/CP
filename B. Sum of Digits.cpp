b0bc9558e294b8d9c2972c0178e6dbb5904094c3
Ahmed Zaher <ahmedzaher5788@gmail.com>
2022-01-08 22:48:13 +0200

    Create B. Sum of Digits.cpp

---
+++ B. Sum of Digits.cpp
@@ -0,0 +1,64 @@
+#include<set>
+#include<map>
+#include<list>
+#include<iomanip>
+#include<cmath>
+#include<string>
+#include<vector>
+#include<queue>
+#include<stack>
+#include<complex>
+#include<sstream>
+#include<iostream>
+#include<algorithm>
+#include<numeric>
+#include<utility>
+#include <functional>
+#include<stdio.h>
+#include<assert.h>
+#include<memory.h>
+#include <bits/stdc++.h>
+using namespace std;
+
+#define all(v)				((v).begin()), ((v).end())
+#define sz(v)				((int)((v).size()))
+#define clr(v, d)			memset(v, d, sizeof(v))
+#define rep(i, v)		    for(int i=0;i<sz(v);++i)
+#define lp(i, n)		    for(int i=0;i<(int)(n);++i)
+#define lpi(i, j, n)	    for(int i=(j);i<(int)(n);++i)
+#define lpd(i, j, n)	    for(int i=(j);i>=(int)(n);--i)
+#define repa(v)			    lpi(i, 0, sz(v)) lpi(j, 0, sz(v[i]))
+#define P(x)				cout<<#x<<" = { "<<x<<" }\n"
+#define pb					push_back
+#define MP					make_pair
+#define el                  endl
+
+typedef vector<int> vi;
+typedef vector<double> vd;
+typedef vector<vi> vvi;
+typedef vector<vd> vvd;
+typedef vector<string> vs;
+typedef long long ll;
+typedef long double ld;
+
+const int OO = (int) 1e8;	//Small -> WRONG, Large -> OVERFLOW
+
+const double PI = acos(-1.0);
+const double EPS = (1e-7);
+
+
+
+int main() {
+    string str;
+    cin>>str;
+    ll cnt = 0;
+    while(str.size() != 1)
+    {
+        ll sum = 0;
+        rep(i, str)sum += str[i] - '0';
+        str=to_string(sum);
+        cnt++;
+    }
+    cout<<cnt;
+    return 0;
+}
